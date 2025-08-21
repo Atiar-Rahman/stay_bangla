b73d0b1238da42e010e07f0f7b080c10bec3f64f
Atiar-Rahman <atiar1709@gmail.com>
2025-08-21 07:31:36 +0600

    swagger doc added

--- staybangla/settings.py
+++ staybangla/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'drf_yasg',
     # 3rd party
     "debug_toolbar",
     'rest_framework',
